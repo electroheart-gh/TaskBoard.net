b752d91d2e97f34912fae61a477dfd29d3bcce22
JUNJI <sgswjj@gmail.com>
2023-11-25 01:15:28 +0900

    First Step

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Board = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Board)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Board
+            // 
+            this.Board.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Board.Location = new System.Drawing.Point(0, 0);
+            this.Board.Name = "Board";
+            this.Board.Size = new System.Drawing.Size(948, 595);
+            this.Board.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.Board.TabIndex = 0;
+            this.Board.TabStop = false;
+            this.Board.Click += new System.EventHandler(this.Board_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(948, 595);
+            this.Controls.Add(this.Board);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Board)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox Board;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,27 @@ namespace TaskBoard.net
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //描画先とするImageオブジェクトを作成する
+            Bitmap canvas = new Bitmap(Board.Width, Board.Height);
+            //ImageオブジェクトのGraphicsオブジェクトを作成する
+            Graphics gr = Graphics.FromImage(canvas);
+
+            //(10,20)の位置に100x80サイズの黒で塗りつぶされた長方形を描画する
+            gr.FillRectangle(Brushes.Black, Board.ClientRectangle);
+
+            //Graphicsオブジェクトのリソースを解放する
+            gr.Dispose();
+            //PictureBox1に表示する
+
+            Board.Image = canvas;
+        }
+
+        private void Board_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
